65085bf5b4579aefe519a0dc8822d22f921a6ee2
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-09-24 23:07:58 +0200

    second

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,17 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\HttpFoundation\JsonResponse;
 
 class MoviesController extends AbstractController
 {
+    // #[Route('/movies/{name}', name: 'app_movies', defaults:['name'=>null], methods: ['GET', 'HEAD'])]
+    // public function index($name): JsonResponse
+    // {
+    //     return $this->json([
+    //         'message' => 'Welcome to your new controller '.$name,
+    //         'path' => 'src/Controller/MoviesController.php'
+    //     ]);
+    // }
     #[Route('/movies', name: 'app_movies')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MoviesController.php',
-        ]);
+    public function index(): Response
+    {   
+        $movies = ["Avengers: Endgame", "Inception", "Loki", "Black Widow"];
+        
+        return $this->render('index.html.twig', 
+        // ['title' => 'Avengers: Endgame']
+         array('movies' => $movies)
+        );
     }
 }
