4005c6e94f38251f03e5c320b925128f021617c9
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-09-27 22:35:55 +0200

    form 1

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
-use App\Repository\MovieRepository;
 use App\Entity\Movie;
+use App\Form\MovieFormType;
+use App\Repository\MovieRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
 // use Symfony\Component\HttpFoundation\JsonResponse;
 
 class MoviesController extends AbstractController
 {   
+    private $em;
     private $movieRepository;
-    public function __construct(MovieRepository $movieRepository) {
+    public function __construct(EntityManagerInterface $em, MovieRepository $movieRepository) 
+    {
+        $this->em = $em;
         $this->movieRepository = $movieRepository;
     }
 
@@ -32,6 +39,65 @@ class MoviesController extends AbstractController
         // ]);
     }
 
+    // #[Route('/movies/create', name: 'create_movie')]
+    // public function create (Request $request): Response {
+        
+    //     $movie = new Movie();
+    //     $form = $this->createForm(MovieFormType::class, $movie);
+
+    //     $form->handleRequest($request);
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $newMovie = $form->getData();
+            
+    //         dd($newMovie);
+    //         exit;
+    //     }
+        
+    //     return $this->render('/movies/create.html.twig', [
+    //         'form' => $form->createView()
+    //     ]);
+    // }
+
+
+    #[Route('/movies/create', name: 'create_movie')]
+    public function create(Request $request): Response
+    {
+        $movie = new Movie();
+        $form = $this->createForm(MovieFormType::class, $movie);
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newMovie = $form->getData();
+            $imagePath = $form->get('imagePath')->getData();
+            
+            if ($imagePath) {
+                $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+
+                try {
+                    $imagePath->move(
+                        $this->getParameter('kernel.project_dir') . '/public/uploads',
+                        $newFileName
+                    );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+                $newMovie->setUserId($this->getUser()->getId());
+                $newMovie->setImagePath('/uploads/' . $newFileName);
+            }
+
+            $this->em->persist($newMovie);
+            $this->em->flush();
+
+            return $this->redirectToRoute('movies');
+        }
+
+        return $this->render('movies/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
     #[Route('/movies/{id}', methods: ['GET'], name: 'app_movie')]
     public function show($id): Response
     {   
@@ -41,7 +107,6 @@ class MoviesController extends AbstractController
             'movie' => $movie
         ]);
     }
-
     
     // #[Route('/movies/{name}', name: 'app_movies', defaults:['name'=>null], methods: ['GET', 'HEAD'])]
     // public function index($name): JsonResponse
